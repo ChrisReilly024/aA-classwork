9560a2e25b2d657c328bf6d1661ad29ec53fcc52
ChrisReilly024 <creilly024@gmail.com>
2020-04-07 17:22:08 -0400

    piece initialized

---
+++ w4d2/03_chess_part_1/piece.rb
@@ -0,0 +1,40 @@
+require_relative 'pawn'
+require_relative 'rook_bishop_queen'
+require_relative 'knight_king'
+require_relative 'null_piece'
+
+class Piece
+
+
+   
+    def initialize(color, board, pos)
+
+        @color = :color
+        @position = []
+        @board = board
+        
+    end
+
+    def to_s
+
+    end
+
+    def empty?
+    end
+
+    def valid_moves
+    end
+
+    def pos=(val)
+    end
+
+    def symbol
+    end
+
+private
+
+    def move_into_check?(end_pos)
+    end
+
+
+end
