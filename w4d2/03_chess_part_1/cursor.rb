2151a461a89c486c8d1895646fa9cf2811f6ed00
ChrisReilly024 <creilly024@gmail.com>
2020-04-08 09:40:45 -0400

    cursor setup

--- w4d2/03_chess_part_1/cursor.rb
+++ w4d2/03_chess_part_1/cursor.rb
@@ -1,5 +1,30 @@
 require_relative 'board'
 
 class Cursor < board
+    def initialize(cursor_pos, board)
+        @cursor_pos = []
+        @board = board
+        @selected = false
+    end
 
+    def get_input
+
+    end
+
+    def toggle_selected
+
+    end
+
+    private
+    def handle_key(key)
+
+    end
+
+    def read_char
+
+    end
+
+    def update_pos(diff)
+
+    end
 end
