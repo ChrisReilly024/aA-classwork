3def3c68531766a018091cb62cf91c23be398066
ChrisReilly024 <creilly024@gmail.com>
2020-04-08 12:14:10 -0400

    move possibilities added, lunch break

--- w4d2/03_chess_part_1/slide_mod.rb
+++ w4d2/03_chess_part_1/slide_mod.rb
@@ -1,21 +1,22 @@
 module Slideable
     
     def horizontal_dirs
-
     end
-
+    
     def diagonal_dirs
-
+        
     end
-
+    
     def moves
-
+        
     end   
-
+    
     private
     
-    HORIZONTAL_DIRS = []
-    DIAGONAL_DIRS = []
+                     # right    up     down     left
+    HORIZONTAL_DIRS = [[1,0], [0,1], [0,-1], [-1,0]]
+                  # rt-up    lt-dw    lt-up   rt-dw
+    DIAGONAL_DIRS = [[1,1], [-1,-1], [-1,1], [1,-1]]
 
     def move_dirs #overwritten by subclass
 
