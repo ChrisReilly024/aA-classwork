a70af700cf4a6e841b2b6b1b6a43a69a5a65f7b4
ChrisReilly024 <creilly024@gmail.com>
2020-04-13 15:12:26 -0400

    p03 completed

--- w5d1/skeleton/lib/p03_hash_set.rb
+++ w5d1/skeleton/lib/p03_hash_set.rb
@@ -1,5 +1,5 @@
 class HashSet
-  attr_reader :count
+  attr_accessor :count, :store
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { Array.new }
@@ -7,18 +7,38 @@ class HashSet
   end
 
   def insert(key)
+    if include?(key)
+      false
+    else
+    index = key.hash % @store.length
+    @store[index] << key
+    @count += 1
+    resize! if count == store.length
+    true
+    end
   end
-
+  
   def include?(key)
+    index = key.hash % @store.length
+    @store[index].include?(key)
   end
 
   def remove(key)
+    if include?(key)
+      index = key.hash % @store.length
+      @store[index].delete(key)
+      @count -= 1
+      true
+    else
+      false
+    end
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    index = num.hash % @store.length
+    self.store[index]
   end
 
   def num_buckets
@@ -26,5 +46,12 @@ class HashSet
   end
 
   def resize!
+    old_ele = @store.flatten
+    @store = Array.new(@store.length * 2) { Array.new }
+
+    old_ele.each do |num|
+      @store[num % @store.length] << num
+    end
+    
   end
 end
