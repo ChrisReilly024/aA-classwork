82ca3347392c2d4b8c3567af77ca831f812ab876
ChrisReilly024 <creilly024@gmail.com>
2020-04-25 23:24:21 -0400

    2 is_sorted completed git push

--- W1D1/02_algorithmic_strategies_exercise/algorithmic_strategies_exercise/02_is_sorted.rb
+++ W1D1/02_algorithmic_strategies_exercise/algorithmic_strategies_exercise/02_is_sorted.rb
@@ -2,7 +2,10 @@
 # The method should return true if the elements are in increasing order, false otherwise.
 # Do not use the built-in Array#sort in your solution :)
 
-
+def is_sorted(arr)
+    arr.inject {|acc,cur| return false if acc > cur; acc = cur}
+    true
+end
 p is_sorted([1, 4, 10, 13, 15])       # => true
 p is_sorted([1, 4, 10, 10, 13, 15])   # => true
 p is_sorted([1, 2, 5, 3, 4 ])         # => false
