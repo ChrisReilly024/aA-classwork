9f730aaa1239500d8c2f57af7f723897ad826bab
ChrisReilly024 <creilly024@gmail.com>
2020-04-27 05:52:44 -0400

    part 2 complete

--- W1D2/1 rspec_exercise_1/rspec_exercise_1/lib/part_2.rb
+++ W1D2/1 rspec_exercise_1/rspec_exercise_1/lib/part_2.rb
@@ -1,6 +1,6 @@
-# def hipsterfy(word)
-#     word.reverse.sub(/[aeiou]/, '').reverse
-# end
+def hipsterfy(word)
+    word.reverse.sub(/[aeiou]/, '').reverse
+end
 
 def vowel_counts(str)
     hash = Hash.new(0)
@@ -8,4 +8,8 @@ def vowel_counts(str)
     hash
 end
 
-vowel_counts("williamsburg bridge")
+def caesar_cipher(str,n)
+    alpha = ('a'..'z').to_a
+    hash = Hash[alpha.zip(alpha.rotate(n))]
+    str.gsub(/[a-z]/, hash)
+end
