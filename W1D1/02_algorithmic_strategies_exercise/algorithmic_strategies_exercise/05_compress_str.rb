cf9bf4cacc93dc3b8a62a5303d2247aa2e7b47d0
ChrisReilly024 <creilly024@gmail.com>
2020-04-26 23:59:01 -0400

    replace char completed

--- W1D1/02_algorithmic_strategies_exercise/algorithmic_strategies_exercise/05_compress_str.rb
+++ W1D1/02_algorithmic_strategies_exercise/algorithmic_strategies_exercise/05_compress_str.rb
@@ -2,6 +2,19 @@
 # The method should return a new str where streaks of consecutive characters are compressed.
 # For example "aaabbc" is compressed to "3a2bc".
 
+def compress_str(str)
+    new_str = ''
+    count = 1
+    str.chars.inject do |char, next_char|
+        if char == next_char
+            count += 1 
+            char = next_char
+        else
+        new_str += count > 1 ? "#{count.to_s}#{char}" : char
+        count = 1
+        end
+    end
+end
 
 p compress_str("aaabbc")        # => "3a2bc"
 p compress_str("xxyyyyzz")      # => "2x4y2z"
