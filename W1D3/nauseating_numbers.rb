6168ec915abd2d78f81fadcc1fad651e1a6143c6
ChrisReilly024 <creilly024@gmail.com>
2020-04-30 08:43:16 -0400

    nn part 1

--- W1D3/nauseating_numbers.rb
+++ W1D3/nauseating_numbers.rb
@@ -59,16 +59,16 @@ def isPerfectSquare(num)
         mid = right - (right-left)/2;
         if num % mid == 0 && mid == num/mid
             return true;
-        elsif mid<num/mid
-            left = mid+1;
+        elsif mid < num/mid
+            left = mid + 1;
         else
-            right = mid-1;
+            right = mid - 1;
         end
     end    
     return false
 end
-
-p isPerfectSquare(49) 
+      
+# p isPerfectSquare(49) 
 
     # public static void main(String[] args) {
     #     int N = 16;
@@ -95,17 +95,25 @@ anti_prime?
 Write a method anti_prime? that accepts a number as an argument. The method should return true if the given number has more divisors than all positive numbers less than the given number. For example, 24 is an anti-prime because it has more divisors than any positive number less than 24. Math Fact: Numbers that meet this criteria are also known as highly composite numbers.
 =end
 
+def num_divisors(num)
+    (1..num).count {|n| num % n == 0}
+end
 
-# p anti_prime?(24)   # true
-# p anti_prime?(36)   # true
-# p anti_prime?(48)   # true
-# p anti_prime?(360)  # true
-# p anti_prime?(1260) # true
-# p anti_prime?(27)   # false
-# p anti_prime?(5)    # false
-# p anti_prime?(100)  # false
-# p anti_prime?(136)  # false
-# p anti_prime?(1024) # false
+def anti_prime?(num)
+    total = num_divisors(num)
+     (1...num).none? { |n| num_divisors(n) >= total }
+end
+
+# p anti_prime?(24)   == true
+# p anti_prime?(36)   == true
+# p anti_prime?(48)   == true
+# p anti_prime?(360)  == true
+# p anti_prime?(1260) == true
+# p anti_prime?(27)   == false
+# p anti_prime?(5)    == false
+# p anti_prime?(100)  == false
+# p anti_prime?(136)  == false
+# p anti_prime?(1024) == false
 
 # matrix_addition
 # Let a 2-dimensional array be known as a "matrix". Write a method matrix_addition that accepts two matrices as arguments. The two matrices are guaranteed to have the same "height" and "width". The method should return a new matrix representing the sum of the two arguments. To add matrices, we simply add the values at the same positions:
@@ -117,11 +125,23 @@ Write a method anti_prime? that accepts a number as an argument. The method shou
 # 2 5  +  9 1  =>  11 6
 # 4 7     3 0      7 7
 
-# matrix_a = [[2,5], [4,7]]
-# matrix_b = [[9,1], [3,0]]
-# matrix_c = [[-1,0], [0,-1]]
-# matrix_d = [[2, -5], [7, 10], [0, 1]]
-# matrix_e = [[0 , 0], [12, 4], [6,  3]]
+def matrix_addition(ar1,ar2)
+    length = ar1.length
+    width = ar1[0].length
+    ar = Array.new(length) {Array.new(width, 0)}
+    (0...length).each do |i|
+        (0...width).each do |j|
+            ar[i][j] = ar1[i][j] + ar2[i][j]
+        end
+    end
+    ar
+end
+
+matrix_a = [[2,5], [4,7]]
+matrix_b = [[9,1], [3,0]]
+matrix_c = [[-1,0], [0,-1]]
+matrix_d = [[2, -5], [7, 10], [0, 1]]
+matrix_e = [[0 , 0], [12, 4], [6,  3]]
 
 # p matrix_addition(matrix_a, matrix_b) # [[11, 6], [7, 7]]
 # p matrix_addition(matrix_a, matrix_c) # [[1, 5], [4, 6]]
@@ -131,6 +151,9 @@ Write a method anti_prime? that accepts a number as an argument. The method shou
 # mutual_factors
 # Write a method mutual_factors that accepts any amount of numbers as arguments. The method should return an array containing all of the common divisors shared among the arguments. For example, the common divisors of 50 and 30 are 1, 2, 5, 10. You can assume that all of the arguments are positive integers.
 
+def mutual_factors(*ar)
+    (1..ar.min).to_a.select {|num| ar.all? {|n| n % num == 0 }}
+end
 
 # p mutual_factors(50, 30)            # [1, 2, 5, 10]
 # p mutual_factors(50, 30, 45, 105)   # [1, 5]
@@ -149,13 +172,19 @@ Write a method anti_prime? that accepts a number as an argument. The method shou
 # 1, 1, 2, 4, 7, 13, ... and so on
 # Write a method tribonacci_number that accepts a number argument, n, and returns the n-th number of the tribonacci sequence.
 
+def tribonacci_number(num)
+    return 1 if num == 1 || num == 2 
+    return 2 if num == 3
+
+    tribonacci_number(num - 1) + tribonacci_number(num - 2) + tribonacci_number(num - 3)
+end
 
 # p tribonacci_number(1)  # 1
 # p tribonacci_number(2)  # 1
 # p tribonacci_number(3)  # 2
 # p tribonacci_number(4)  # 4
 # p tribonacci_number(5)  # 7
-# p tribonacci_number(6)  # 13
+p tribonacci_number(6)  # 13
 # p tribonacci_number(7)  # 24
 # p tribonacci_number(11) # 274
 
