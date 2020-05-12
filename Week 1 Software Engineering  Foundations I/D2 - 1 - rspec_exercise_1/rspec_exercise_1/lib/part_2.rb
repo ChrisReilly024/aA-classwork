d31eb93aa03d61cb915dfaeeec1112181b924094
ChrisReilly024 <creilly024@gmail.com>
2020-05-12 13:48:53 -0400

    project 1 complete

--- Week 1 Software Engineering  Foundations I/D2 - 1 - rspec_exercise_1/rspec_exercise_1/lib/part_2.rb
+++ Week 1 Software Engineering  Foundations I/D2 - 1 - rspec_exercise_1/rspec_exercise_1/lib/part_2.rb
@@ -3,9 +3,7 @@ def hipsterfy(word)
     i = word.length - 1
     while i > 0
         char = word[i]
-        if vowels.include?(char)
-            return word[0...i] + word[i+1..-1]
-        end
+        return word[0...i] + word[i+1..-1] if vowels.include?(char)
         i -= 1
     end
     word
@@ -19,3 +17,23 @@ def vowel_counts(str)
     end
     count
 end
+# str.each_char {|char| count[char.downcase] += 1 if char.downcase.match(/[aeiou]/)}
+
+
+def caesar_cipher(str,num)
+    alpha = ('a'..'z').to_a
+    new_str = ''
+    str.each_char do |char|
+        if alpha.include?(char)
+            new_idx = (alpha.index(char) + num) % 26
+            new_str += alpha[new_idx]
+        else
+            new_str += char
+        end
+    end
+    new_str
+end
+
+
+# hash = Hash[alpha.zip(alpha.rotate(num))]
+# str.gsub(/\w/) {|char| hash[char] ? hash[char] : char}
