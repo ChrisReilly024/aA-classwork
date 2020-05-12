cc359c608e4b1a3776e36de66f150f4ecee238fd
ChrisReilly024 <creilly024@gmail.com>
2020-05-12 12:16:33 -0400

    1.5 part 2 complete

--- Week 1 Software Engineering  Foundations I/D2 - 1 - rspec_exercise_1/rspec_exercise_1/lib/part_2.rb
+++ Week 1 Software Engineering  Foundations I/D2 - 1 - rspec_exercise_1/rspec_exercise_1/lib/part_2.rb
@@ -1,3 +1,21 @@
 def hipsterfy(word)
-    
+    vowels = 'aeiou'
+    i = word.length - 1
+    while i > 0
+        char = word[i]
+        if vowels.include?(char)
+            return word[0...i] + word[i+1..-1]
+        end
+        i -= 1
+    end
+    word
+end
+
+def vowel_counts(str)
+    count = Hash.new(0)
+    vowels = 'aeiou'
+    str.each_char do |char|
+        count[char.downcase] += 1 if vowels.include?(char.downcase)
+    end
+    count
 end
