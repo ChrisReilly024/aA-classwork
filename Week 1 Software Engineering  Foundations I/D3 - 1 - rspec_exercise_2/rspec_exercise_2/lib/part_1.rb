0c67e9d5a1b5445050d12756a2ac6eb3fd6984c8
ChrisReilly024 <creilly024@gmail.com>
2020-05-13 12:44:35 -0400

    part 1 edit

--- Week 1 Software Engineering  Foundations I/D3 - 1 - rspec_exercise_2/rspec_exercise_2/lib/part_1.rb
+++ Week 1 Software Engineering  Foundations I/D3 - 1 - rspec_exercise_2/rspec_exercise_2/lib/part_1.rb
@@ -1,13 +1,9 @@
 def partition(arr, num)
     less = []
     greater = []
-        arr.each do |ele|
-            if ele < num
-                less << ele
-            else
-                greater << ele
-            end
-        end
+
+    arr.each {|ele| ele >= num ? greater << ele : less << ele }
+
     [less, greater]
 end
 
