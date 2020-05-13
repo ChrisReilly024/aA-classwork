b40e6f0071f5f3d0e33a080511ed646cb06bae66
ChrisReilly024 <creilly024@gmail.com>
2020-05-13 12:42:22 -0400

    rspec 2 part 1 complete

---
+++ Week 1 Software Engineering  Foundations I/D3 - 1 - rspec_exercise_2/rspec_exercise_2/lib/part_1.rb
@@ -0,0 +1,32 @@
+def partition(arr, num)
+    less = []
+    greater = []
+        arr.each do |ele|
+            if ele < num
+                less << ele
+            else
+                greater << ele
+            end
+        end
+    [less, greater]
+end
+
+def merge(hash_1, hash_2)
+    hash_1.merge(hash_2)    
+end
+
+def swapper(word)
+    word.gsub(/[aeiouAEIOU]/,"*")
+end
+
+def censor(str, arr)
+    str.split.map {|word| arr.include?(word.downcase) ? swapper(word) : word }.join(' ')
+end
+
+def power_of_two?(num)
+    return true if num == 1
+    (2...num).each do |n|
+        return true if 2 ** n == num
+        return false if 2 ** n > num            
+    end
+end
