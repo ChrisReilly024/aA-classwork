722f0ff9e73ddc4da1b1d20bcb9c7e3cac1e57e3
ChrisReilly024 <creilly024@gmail.com>
2020-05-15 13:53:20 -0400

    bootcamp project complete

--- Week 2 Software Engineering Foundations II/D1 - 2 - bootcamp_project/bootcamp_project/lib/bootcamp.rb
+++ Week 2 Software Engineering Foundations II/D1 - 2 - bootcamp_project/bootcamp_project/lib/bootcamp.rb
@@ -1,6 +1,6 @@
 class Bootcamp
 
-    def initialize (name,slogan, student_capacity)
+    def initialize(name,slogan, student_capacity)
         @name = name
         @slogan = slogan
         @student_capacity = student_capacity
@@ -25,36 +25,63 @@ def students
     @students
 end
 
-def hire (string)
-    teachers << string
+def hire(name)
+    teachers << name
 end
 
 
-def enroll(string)
+def enroll(name)
     if students.length < @student_capacity
-        student_capacity << string
+        students << name
         return true
     else
         return false
     end
 end
 
-def enrolled?(string)
-    students.include?(string)
+def enrolled?(name)
+    students.include?(name)
 end
 
 
-def student_to_teacher_ratio(n)
-    students.length / teachers.length 
+def student_to_teacher_ratio
+    (@students.length / @teachers.length).floor
 end
 
 def add_grade(name, num)
-    if enrolled(name)
-        grades[name] << num
+    if enrolled?(name)
+        @grades[name] << num
         return true
     else
         return false
     end
 end
 
-end 
+    def num_grades(student)
+        @grades[student].length
+    end 
+
+    def average_grade(student)
+        if !enrolled?(student) || @grades[student].length == 0
+            return nil
+        else
+            (@grades[student].sum / @grades[student].length).floor
+        end
+    end
+end
+
+    # #student_to_teacher_ratio
+    # should return an integer representing the ratio between # students to 1 teacher rounded down to the nearest integer (FAILED - 1)
+    # #add_grade
+    #   should accept a student (string) and a grade (number) and add that grade to the student's grades array inside of the @grades hash
+    #   when the student is already enrolled
+    #     should add the grade to the student's grades array inside of the @grades hash (FAILED - 2)
+    #     should return true (FAILED - 3)
+    #   when the student is not enrolled in the bootcamp
+    #     should not add the grade (FAILED - 4)
+    #     should return false
+    # #num_grades
+    #   should accept a student (string) and return the number of grades they have (FAILED - 5)
+    # #average_grade
+    #   should accept a student (string) and return a number representing their average grade rounded down to the nearest integer (FAILED - 6)
+    #   should return nil if the student has no grades or is not enrolled (FAILED - 7)
