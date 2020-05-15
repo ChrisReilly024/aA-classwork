a4fe45a349d6daf1e4870f0c3b7431ae524467c5
ChrisReilly024 <creilly024@gmail.com>
2020-05-15 13:04:30 -0400

    bootcamp project

--- Week 2 Software Engineering Foundations II/D1 - 2 - bootcamp_project/bootcamp_project/lib/bootcamp.rb
+++ Week 2 Software Engineering Foundations II/D1 - 2 - bootcamp_project/bootcamp_project/lib/bootcamp.rb
@@ -1,3 +1,60 @@
 class Bootcamp
+
+    def initialize (name,slogan, student_capacity)
+        @name = name
+        @slogan = slogan
+        @student_capacity = student_capacity
+        @teachers = []
+        @students = []
+        @grades = Hash.new {|h,k| h[k] = []}
+    end
   
+def name
+    @name
 end
+
+def slogan
+    @slogan
+end
+
+def teachers
+    @teachers
+end
+
+def students
+    @students
+end
+
+def hire (string)
+    teachers << string
+end
+
+
+def enroll(string)
+    if students.length < @student_capacity
+        student_capacity << string
+        return true
+    else
+        return false
+    end
+end
+
+def enrolled?(string)
+    students.include?(string)
+end
+
+
+def student_to_teacher_ratio(n)
+    students.length / teachers.length 
+end
+
+def add_grade(name, num)
+    if enrolled(name)
+        grades[name] << num
+        return true
+    else
+        return false
+    end
+end
+
+end 
