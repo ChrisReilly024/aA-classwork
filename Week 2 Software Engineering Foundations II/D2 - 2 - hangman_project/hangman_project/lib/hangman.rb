150166b400d2e839c10ad7904ace521bfd5531fb
ChrisReilly024 <creilly024@gmail.com>
2020-05-18 14:19:12 -0400

    hangman part 1 complete

--- Week 2 Software Engineering Foundations II/D2 - 2 - hangman_project/hangman_project/lib/hangman.rb
+++ Week 2 Software Engineering Foundations II/D2 - 2 - hangman_project/hangman_project/lib/hangman.rb
@@ -1,7 +1,29 @@
 class Hangman
   DICTIONARY = ["cat", "dog", "bootcamp", "pizza"]
 
+  attr_reader :guess_word, :attempted_chars, :remaining_incorrect_guesses
   def self.random_word
     DICTIONARY.sample
   end
+
+  def initialize()
+    @secret_word = Hangman.random_word
+    @guess_word = Array.new(@secret_word.length,'_')
+    @attempted_chars = []
+    @remaining_incorrect_guesses = 5
+  end
+
+  def already_attempted?(char)
+    @attempted_chars.include?(char)
+  end
+
+  def get_matching_indices(char)
+    chars = @secret_word.chars
+    chars.map.with_index { |ch,i| i if ch == char }.compact
+  end
+
+  def fill_indices(char, indices)
+    indices.each {|i| @guess_word[i] = char}
+  end
+
 end
