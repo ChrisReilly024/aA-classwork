7d9790e7c19e606a0bf4c22b456ad2abfdb6ceb7
ChrisReilly024 <creilly024@gmail.com>
2020-05-18 14:51:00 -0400

    room complete

--- Week 2 Software Engineering Foundations II/D2 - 3 - hotel_project/hotel_project/lib/room.rb
+++ Week 2 Software Engineering Foundations II/D2 - 3 - hotel_project/hotel_project/lib/room.rb
@@ -1,3 +1,25 @@
 class Room
+    attr_reader :capacity, :occupants
+  def initialize(capacity)
+    @capacity = capacity
+    @occupants = []
+  end
 
+    def full?
+        @occupants.count < @capacity ? false : true
+    end
+
+    def available_space
+        @capacity - @occupants.count
+    end
+
+    def add_occupant(name)
+        if !self.full?
+            @occupants << name
+            return true
+        else
+            return false
+        end
+    end
+    
 end
