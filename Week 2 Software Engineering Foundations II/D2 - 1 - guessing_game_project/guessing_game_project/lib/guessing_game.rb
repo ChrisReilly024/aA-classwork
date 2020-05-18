56b6fad2b7d1942d08722d9cf4c13a6566034440
ChrisReilly024 <creilly024@gmail.com>
2020-05-18 13:58:37 -0400

    guessing game complete

--- Week 2 Software Engineering Foundations II/D2 - 1 - guessing_game_project/guessing_game_project/lib/guessing_game.rb
+++ Week 2 Software Engineering Foundations II/D2 - 1 - guessing_game_project/guessing_game_project/lib/guessing_game.rb
@@ -1,3 +1,51 @@
 class GuessingGame
+    attr_reader :check_num, :num_attempts
+
+    def initialize(min,max)
+        @min = min 
+        @max = max
+        @secret_num = rand(min..max)
+        @num_attempts = 0
+        @game_over = false
+    end
+
+    def game_over?
+        @game_over
+    end
+
+    def check_num(num)
+        @num_attempts += 1
+        if num == @secret_num
+            @game_over = true
+            p "you win"
+        elsif num > @secret_num
+            p 'too big'
+        elsif num < @secret_num
+            p 'too small'
+        end
+    end
+
+    def ask_user
+        puts "Enter a number: "
+        input = gets.chomp.to_i
+        self.check_num(input)
+    end
+    
 
 end
+# 1) GuessingGame #check_num should set @game_over to true if the num is equal to @secret_num
+# Failure/Error: expect(guessing_game.game_over?).to eq(true)
+  
+#   expected: true
+#        got: false
+  
+#   (compared using ==)
+# # ./spec/guessing_game_spec.rb:63:in `block (3 levels) in <top (required)>'
+
+# 2) GuessingGame #check_num when the num is equal to @secret_num should print 'you win'
+# Failure/Error: expect { guessing_game.check_num(42) }.to output(/win/).to_stdout
+#   expected block to output /win/ to stdout, but output "\"too small\"\n"
+#   Diff:
+#   @@ -1,2 +1,2 @@
+#   -/win/
+#   +"too small"
