f1896a1a57f04767107fee4ff4de3d3a626c327f
ChrisReilly024 <creilly024@gmail.com>
2020-06-01 15:05:34 -0400

    initialize, move constant

--- Week 3 Ruby I/D5 - 3 - knights traversal/D53knightstraversal/knightstraversals.rb
+++ Week 3 Ruby I/D5 - 3 - knights traversal/D53knightstraversal/knightstraversals.rb
@@ -1,5 +1,36 @@
 class KnightPathFinder
+    MOVES = [
+        [-2, -1],
+        [-2,  1],
+        [-1, -2],
+        [-1,  2],
+        [ 1, -2],
+        [ 1,  2],
+        [ 2, -1],
+        [ 2,  1]
+      ]
+
+    def self.valid_moves(pos)
+        valids = [] #valid moves go here
+        x,y = pos #our current posistion
+        # will need to loop through move list
+        # add those to current and see if it falls within board
+        # if so, new pos goes into valid moves
+        MOVES.each do |px, py|
+            new_pos = []
+            if px + x > 
+        end
+    end
+
+    attr_reader :start, :board
     def initialize(start)
-        @start = start
+        @board = Array.new(8) {Array.new(8,'-')}
+        @start = @board[start]
+    end
+
+    def build_move_tree(@start)
+
     end
 end
+
+
