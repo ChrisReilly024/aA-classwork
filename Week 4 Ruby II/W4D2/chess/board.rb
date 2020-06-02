495df3e940f70bed83f2fcf5e4b925025dd79587
ChrisReilly024 <creilly024@gmail.com>
2020-06-02 15:05:48 -0400

    slideable module setup, horizont + diagonal dirs

--- Week 4 Ruby II/W4D2/chess/board.rb
+++ Week 4 Ruby II/W4D2/chess/board.rb
@@ -11,6 +11,33 @@ class Board
 
     def []=(pos, val)
         row, col = pos
+        @rows[row][col] = val
+    end
+
+    def move_piece(color, start_pos, end_pos)
+    end
+
+    def valid_pos?(pos)
         
+    end
+
+    def add_piece(piece, pos)
+    end
+
+    def checkmate?(color)
+    end
+
+    def in_check?(color)
+    end
+
+    def find_king(color)
+    end
+
+    def pieces
+    end
+
+    def dup
+
+
 
 end
--- Week 4 Ruby II/W4D2/chess/slideable.rb
+++ Week 4 Ruby II/W4D2/chess/slideable.rb
@@ -1,2 +1,4 @@
 class Slideable
+    HORIZONTAL_DIRS = [[1,0], [0,1], [-1,0], [0,-1]]
+    DIAGONAL_DIRS = [[1,1], [-1,1], [1,-1], [-1,-1]]
 end
