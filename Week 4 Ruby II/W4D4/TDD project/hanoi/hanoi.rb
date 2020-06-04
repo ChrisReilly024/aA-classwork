2afecf7e2c71a2aad51e508767e220e22eb644a1
ChrisReilly024 <creilly024@gmail.com>
2020-06-04 19:06:49 -0400

    initializer method + factory complete

--- Week 4 Ruby II/W4D4/TDD project/hanoi/hanoi.rb
+++ Week 4 Ruby II/W4D4/TDD project/hanoi/hanoi.rb
@@ -1,5 +1,17 @@
 class Hanoi
-    def initialize
-
+    def initialize(stacks, stones)
+        raise InitError.new('stacks and stones must be an integer') if !stacks.is_a?(Integer) || !stones.is_a?(Integer)
+        @stacks = stacks
+        @stones = stones
+        @board = Array.new(stacks) {Array.new}
+        set_game
     end
+    
+    def set_game        
+        for i in 0...stones do
+            @board[0] << i + 1
+            @board[i] = [] if !@board[i].nil? && i != 0
+        end
+    end
+    
 end
