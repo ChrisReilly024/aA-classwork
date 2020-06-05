7bd548b17b85fa47f8cc708907ac5b92cacc3487
ChrisReilly024 <creilly024@gmail.com>
2020-06-05 07:42:05 -0400

    get move, valid move methods

--- Week 4 Ruby II/W4D4/TDD project/hanoi/hanoi.rb
+++ Week 4 Ruby II/W4D4/TDD project/hanoi/hanoi.rb
@@ -1,3 +1,5 @@
+class InitError < StandardError; end
+
 class Hanoi
     def initialize(stacks, stones)
         raise InitError.new('stacks and stones must be an integer') if !stacks.is_a?(Integer) || !stones.is_a?(Integer)
@@ -7,11 +9,30 @@ class Hanoi
         set_game
     end
     
+    def prompt
+        ['Invalid input, try again', 'Invalid move, stack error', "Move piece from which stack to which stack? input format should be #,# ex. (1,2)"]
+    end
+
     def set_game        
         for i in 0...stones do
             @board[0] << i + 1
             @board[i] = [] if !@board[i].nil? && i != 0
         end
     end
-    
+
+    def get_move
+        puts prompt[2]
+        input = gets.chomp.split(',').map(&:i)
+        input
+    end
+
+    def valid_move?(input)
+        if @board[input[0]].first > @board[input[1]]
+            puts prompt[1]
+            return false
+        else
+            return true
+        end
+    end
 end
+
