399f5e762374cb66a86812a8f5e6e7ee90b369da
ChrisReilly024 <creilly024@gmail.com>
2020-06-05 16:48:48 -0400

    relocate

--- Week 4 Ruby II/W4D4/hanoi/hanoi.rb
+++ Week 4 Ruby II/W4D4/hanoi/hanoi.rb
@@ -100,19 +100,3 @@ class Hanoi
         return false
     end
 end
-
-#----------------------------------------
-# def set_game        
-#     for i in 0...stones do
-#         @board[0] << i + 1
-#         @board[i] = [] if !@board[i].nil? && i != 0
-#     end
-# end
-
-#def initialize(stacks, stones)
-    # raise InitError.new('stacks and stones must be an integer') if !stacks.is_a?(Integer) || !stones.is_a?(Integer)
-    # @stacks = stacks
-    # @stones = stones
-    # @board = Array.new(stacks) {Array.new}
-    # set_game
-#end
